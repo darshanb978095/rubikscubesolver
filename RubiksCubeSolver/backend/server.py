73aac7b0733b07a12565ead63aa19be55069d76f
darshanb978095 <darshan.978095@gmail.com>
2026-06-11 14:33:31 +0530

    Configure Render deployment

--- RubiksCubeSolver/backend/server.py
+++ RubiksCubeSolver/backend/server.py
@@ -168,4 +168,5 @@ def solve():
 
 if __name__ == "__main__":
     print("Rubik's Cube Solver is running at http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
